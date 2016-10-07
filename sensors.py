d0d2623089c326e2c03a354b809630f79ad0fa4a
zeratul2099 <zeratul@posteo.de>
2016-10-07 15:46:35 +0000

    added initial sensors.py

---
+++ sensors.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import serial
+
+
+# sensor name mapping
+sensor_map = {
+    '1': 'Sensor 1',
+    '2': 'Sensor 2',
+    '3': 'Sensor 3',
+}
+
+DEVICE = '/dev/ttyS0'
+ 
+def main():
+    while(True):
+        ser = serial.Serial(DEVICE, 9600)
+        line = ser.readline().strip()
+        print('Received input: ' + line)
+        splitList=line.split(":")
+        keys = line.split(':')[0::2]
+        values = line.split(':')[1::2]
+        value_dict = dict(zip(keys, values))
+        try:
+            assert('ID' in keys and 'TS' in keys)
+        except AssertionError:
+            print('invalid input')
+            continue
+        sensor = sensor_map.get(value_dict['ID'], 'DeviceID' + value_dict['ID'])
+        print('Sensor: %s, Temperature: %s, Humidity: %s' % (sensor, value_dict['TC'], value_dict['RH']))
+
+if __name__ == '__main__':
+    main()
