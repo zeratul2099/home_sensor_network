6d58995733d2d1378eacc31b7529cd01a7bdfe9f
Marko Krause <zeratul@posteo.de>
2016-10-08 23:15:44 +0200

    get_database to common

---
+++ common.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine, Table, MetaData, Column, String, Integer, Float, DateTime
+from sqlalchemy.exc import OperationalError
+
+def get_database():
+    db = create_engine(database)
+    metadata = MetaData(db)
+    log = Table('sensor_log', metadata,
+                Column('sensor_id', Integer, primary_key=True),
+                Column('sensor_name', String(128)),
+                Column('timestamp', DateTime, primary_key=True),
+                Column('temperature', Float),
+                Column('humidity', Float),
+                )
+    try:
+        log.create()
+    except OperationalError:
+        # import traceback
+        # traceback.print_exc()
+        pass
+    return log
--- sensors.py
+++ sensors.py
@@ -2,29 +2,10 @@
 from datetime import datetime
 import serial
 
-from sqlalchemy import create_engine, Table, MetaData, Column, String, Integer, Float, DateTime
-from sqlalchemy.exc import OperationalError
-
+from common import get_database
 from settings import device, database, sensor_map
 
 
-def get_database():
-    db = create_engine(database)
-    metadata = MetaData(db)
-    log = Table('sensor_log', metadata,
-                Column('sensor_id', Integer, primary_key=True),
-                Column('sensor_name', String(128)),
-                Column('timestamp', DateTime, primary_key=True),
-                Column('temperature', Float),
-                Column('humidity', Float),
-                )
-    try:
-        log.create()
-    except OperationalError:
-        # import traceback
-        # traceback.print_exc()
-        pass
-    return log
 
 
 def main():
